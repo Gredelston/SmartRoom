b346a61fd29b512bc496b9666454e6ec4e6c0b23
Gredelston <greg@students.olin.edu>
2013-08-31 16:15:06 -0400

    Now switches stations without bugs

--- pandora.py
+++ pandora.py
@@ -1,53 +1,117 @@
 import pexpect
+import os
 
 class Player:
     
     def __init__(self, playIndex=None):
         """
         When the Player is instantiated, spawn pianobar in the terminal.
-        By default, assume we don't want any music playing.
-        This can be overridden with the optional argument playIndex.
+        If music defaults to playing, pause it and go to the station select screen.
         """
 
-        self.child = pexpect.spawn('pianobar') # spawn the pianobar
+        # First things first, clear that pesky state config file.
+        # Otherwise, pianobar will default to playing a station,
+        # which just throws everything off.
+        if os.path.isfile(os.path.expanduser("~" + "/.config/pianobar/state")):
+            print "Clearing state config file."
+            os.system("rm ~/.config/pianobar/state")
 
-        # We don't know whether it will start with a station already playing,
-        # so allow for either situation
-        i = self.child.expect(['#', 'Select station:'])
-
-        if i==0: # Station is already playing.
-            self.child.sendline('S') # Pause
-            self.child.sendline('s') # Change station
-            self.expect('Select station:')
-        if i==1: # At station select screen.
-            pass # everything is A-OK
+        # Spawn the Pianobar
+        self.child = pexpect.spawn('pianobar')
 
         # Now that we're waiting for a station, get the station dict.
         self.updateStations()
 
-        # If requested, play the specified station.
-        if playIndex != None and isinstance(x, int):
-            self.child.sendline(playIndex)
-            self.waitingForStation = False
-            self.currentStationIndex = playIndex
-            self.playing = True
-        else: # No station was requested
+        # Initialize a few variables up in this bitch.
+        self.waitingForStation = True
+        self.currentStationIndex = -1 # By convention, this means no station yet.
+        self.playing = False
+
+
+
+    def playByIndex(self, index):
+        """
+        Takes in the index of a station to be played, and plays that station.
+        """
+
+        # Make sure the index supplied is valid.
+        if not self.validateIndex(index):
+            print "ERROR: Please supply a valid station number!"
+            return
+
+        # Make sure we're waiting for station.
+        if not self.waitingForStation:
+            print 'Switching to station select.'
+            self.child.send('s') # Change station
             self.waitingForStation = True
-            self.currentStationIndex = -1
-            self.playing = False
+        else:
+            print 'Already at station select.'
+
+        self.child.sendline(str(index))
+        self.playing = True
+        self.waitingForStation = False
+        self.currentStationIndex = int(index)
+
+        print "Now playing station: " + self.indexDict[str(index)]
+
+    def quit(self):
+        """
+        Exits gracefully from the Pianobar.
+        """
+        if self.waitingForStation:
+            self.child.send('\n')
+            self.child.expect('#')
+            self.waitingForStation = False
+            self.currentStationIndex = 0
+        print "Exiting from Pandora player."
+        self.child.sendline('q')
+
+    def exit(self):
+        self.quit()
+
+    def validateIndex(self, index):
+        """
+        Returns True if index is a viable station index,
+        and False if not.
+        """
+        return str(index) in self.indexDict.keys()
 
     def updateStations(self):
         """
         To be called whenever we reach the Station Select screen.
         Creates self.stationDict, which contains all stations:
         {stationTitle: stationIndex}
+        and self.indexDict, which does it backward:
+        {stationIndex: stationTitle}
         """
-        stationList = [i for i in self.child.before.split('\r\n') if "q" in i or "Q" in i]
+
+        self.child.expect('station:')
+
+        lines = self.child.before.split('\r\n')
+        stationList = [line for line in lines if "q" in line or "Q" in line]
         stationDict = {}
+        indexDict = {}
+
         for i in stationList:
-            print "ITEM: " + i
-            stationDict[" ".join(i.split()[3:])] = i.split()[1][:-1]
-        print "Station dict: " , stationDict
+            name = " ".join(i.split()[3:])
+            index = i.split()[1][:-1]
+            stationDict[name] = index
+            indexDict[index] = name
+
         self.stationDict = stationDict
+        self.indexDict = indexDict
+
+
 
 player = Player()
+player.playByIndex(2)
+
+import time
+time.sleep(10)
+print ''
+
+player.playByIndex(4)
+
+time.sleep(10)
+
+player.quit()
