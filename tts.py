4f4d0c16bdde150a005959318e91c64baee4e79f
Gredelston <greg@students.olin.edu>
2013-09-01 01:11:31 -0400

    Made safer against rogue quotation marks

--- tts.py
+++ tts.py
@@ -1,8 +1,9 @@
 import os
 
 def say(string):
+    string = string.replace('"','')
+    string = string.replace('\'','')
     os.system('echo "%s" | festival --tts' % string)
 
-say("Hello")
-say("World")
-say("Hello World")
+if __name__ == "__main__":
+    say("Look, it works! Even with 'apostrophes' and \"quotes\".")
